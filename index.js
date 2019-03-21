b2826ca620560aea2eea8ddf9cbecba9de6dd928
brooksvinyard <8202653+brooksvinyard@users.noreply.github.com>
2019-03-21 10:18:00 -0700

    Make port dynamic

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+require('dotenv').config();
 const server = require('./api/server.js');
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+// We need to make the port dynamic when deploying to Heroku, OR in the local environment use port 4000
+const port = process.env.PORT || 4000;
+const greeting = process.env.GREETING;
+server.listen(port, () => {
+  console.log(`\n*** ${greeting} http://localhost:${port} ***\n`);
 });
